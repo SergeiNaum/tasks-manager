99b9248fd96c8a1753aab818c1e8adee2de2a136
SergeiNaum <sergei.naumencko@yandex.ru>
2024-01-04 13:13:52 +0300

    change db config in settings.py for test

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': os.getenv('DB_NAME'),
@@ -103,7 +103,7 @@ DATABASES = {
     #     'PORT': os.getenv('DB_PORT'),
     # }
 
-    # 'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 }
 
