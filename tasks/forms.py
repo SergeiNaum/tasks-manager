07b15dd57e95bbd6317a58bf6dce776cd38d3afb
SergeiNaum <sergei.naumencko@yandex.ru>
2024-01-08 18:28:28 +0300

    fix TaskForm

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,20 +1,11 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
+from django.contrib.auth import get_user_model
 from tasks import models
 from users.models import User
 
 
 class TaskForm(forms.ModelForm):
-
-    def clean_executor(self):
-        user_id = self.cleaned_data["executor"]
-        user = User.objects.get(id=user_id)
-        return user
-
-    executor = forms.ChoiceField(
-        choices=[("", "---------")] + [(e.id, e.fullname) for e in User.objects.all()],
-        label=_("Executor"))
 
     class Meta:
         model = models.Task
--- users/models.py
+++ users/models.py
@@ -8,3 +8,6 @@ class User(AbstractUser):
     @property
     def fullname(self):
         return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.get_full_name()
